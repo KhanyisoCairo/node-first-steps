bed7f80638c705d8950126fbe961a8fb3adaba39
KhanyisoCairo <khanyisoyezo12@gmail.com>
2019-09-09 16:36:01 +0200

    changes

--- greet_xola.js
+++ greet_xola.js
@@ -1,6 +1,20 @@
 const chalk = require('chalk');
 //import the greet module that is in the current folder
 const greet = require('./greet');
+const figlet = require('figlet')
 
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
-console.log(styledMessage)
+
+
+figlet(greet('Xola'), function (err, data) {
+
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    const styledMessage = chalk.bgRed.black(data);
+    console.log(styledMessage)
+});
+
+
+
